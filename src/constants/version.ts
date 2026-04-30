37dc60bd3ca30950bf6b03c38af3cbf2f76bd5fa
Anindya <86872566+Rover1218@users.noreply.github.com>
2026-04-30 15:05:58 +0530

    feat: add LATEST_ENGINE_VERSION and DOWNLOAD_LINK constants for version 0.1.6

--- src/constants/version.ts
+++ src/constants/version.ts
@@ -1,2 +1,2 @@
-export const LATEST_ENGINE_VERSION = "0.1.5";
-export const DOWNLOAD_LINK = "https://github.com/Rover1218/vortex/releases/download/0.1.5/Vortex-Setup-0.1.0.exe";
+export const LATEST_ENGINE_VERSION = "0.1.6";
+export const DOWNLOAD_LINK = "https://github.com/Rover1218/vortex/releases/download/0.1.6/Vortex-Setup-0.1.6.exe";
